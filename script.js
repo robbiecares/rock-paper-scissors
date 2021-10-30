1175be862aa6b3676128f32815c8b8ee2123e7b2
robbiecares <robbiecares@gmail.com>
2021-10-31 00:01:02 +0200

    basic ui established, missing scoring

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-choices = ['rock', 'paper', 'scissors']
-
-function computerPlay() {
+function pcPlay() {
     // randomly choose a game object for the computer
+    choices = ['rock', 'paper', 'scissors']
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
+// pre-ui function
 function userPlay() {
     // confirm the user's game object
     
@@ -13,7 +13,7 @@ function userPlay() {
     
     while(invalid) {
         //capture user's selection
-        response = prompt("Choose 'rock', 'paper' or 'scissors'")
+        // response = prompt("Choose 'rock', 'paper' or 'scissors'")
 
         // verify users's selection is valid & if so break the loop (clicking 'cancel' gives a 'null' response)
         if (response === null || choices.includes(response.toLowerCase())) {
@@ -35,60 +35,101 @@ function playRound(playerSelection, computerSelection) {
 
     // tie case
     if (playerSelection === computerSelection) {
-        console.log(`the computer also chose ${playerSelection}!`)
-        result = 'tie'
+        // console.log(`the computer also chose ${playerSelection}!`)
+        result = "it's a tie"
+        // result = 'tie'
     
     // winning cases
     } else if ( (playerSelection === 'rock' && computerSelection === 'scissors') ||
                 (playerSelection === 'paper' && computerSelection === 'rock') ||
                 (playerSelection === 'scissors' && computerSelection === 'paper')) {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}!`)
-        result = 'user'
+        result = `You win! ${playerSelection} beats ${computerSelection}!`
+        // result = 'user'
 
     } else {
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}!`)
-        result = 'pc'
+        result = (`You lose! ${computerSelection} beats ${playerSelection}!`)
+        // result = 'pc'
     }
     return result
 }
-     
-function game() {
-    // play five rounds of rps
-    let userWins = 0
-    let pcWins = 0
-    let roundsPlayed = 0
 
-    // set condition for continuing the game
-    keepPlaying = true
-    while(keepPlaying) {
+function playRoundWithButtons(e) {    
 
-        // confirm user would like to continue playing
-        userChoice = userPlay()
-        if (userChoice) {
-            // play round of game
-            result = playRound(userChoice, computerPlay())
-        
-            // update score
-            if (result === 'user') {++userWins};
-            if (result === 'pc') {++pcWins};
-            ++roundsPlayed
-        
-            // stop game after five rounds
-            if (roundsPlayed >= 5)
-                keepPlaying = false
-        } else {
-            // stop game as user selected 'cancel'
-            keepPlaying = false
-            alert('game ended')
-        }
-    }
+//variable to hold the results & user response of the game
+let result;
+let msg
 
-    // summarize results of the game
-    if (roundsPlayed) {
-        let summary = `User Wins: ${userWins}\nPC Wins: ${pcWins}\nTies: ${roundsPlayed - userWins - pcWins}`
-        let status = (userWins === pcWins) ? "It's a tie!" : (userWins > pcWins) ? "You won!" : "You lost!"      
-        alert(`${status}\n\n${summary}`)        
-    }
+// confirm user's choice
+const playerSelection = this.textContent.toLowerCase()
+
+
+// confirm pc's choice
+const pcSelection = pcPlay()
+    
+// logic for winning/losing/tie-ing
+
+// tie case
+if (playerSelection === pcSelection) {
+    result = 'tie'
+    msg = `The computer also chose ${playerSelection}, it's a ${result}!`
+
+
+// winning cases
+} else if ( (playerSelection === 'rock' && pcSelection === 'scissors') ||
+            (playerSelection === 'paper' && pcSelection === 'rock') ||
+            (playerSelection === 'scissors' && pcSelection === 'paper')) {
+    result = 'win'
+    msg = `The computer chose ${pcSelection}, you ${result}!`
+
+} else {
+    result = 'lose'
+    msg = `The computer chose ${pcSelection}, you ${result}!`
 }
 
-game()
+// pass msg to result div
+console.log(document.querySelector('.result').textContent = msg)
+
+return result
+}
+
+// function game() {
+//     // play five rounds of rps
+//     let userWins = 0
+//     let pcWins = 0
+//     let roundsPlayed = 0
+
+//     // set condition for continuing the game
+//     keepPlaying = true
+//     while(keepPlaying) {
+
+//         // confirm user would like to continue playing
+//         userChoice = userPlay()
+//         if (userChoice) {
+//             // play round of game
+//             result = playRound(userChoice, computerPlay())
+        
+//             // update score
+//             if (result === 'user') {++userWins};
+//             if (result === 'pc') {++pcWins};
+//             ++roundsPlayed
+        
+//             // stop game after five rounds
+//             if (roundsPlayed >= 5)
+//                 keepPlaying = false
+//         } else {
+//             // stop game as user selected 'cancel'
+//             keepPlaying = false
+//             alert('game ended')
+//         }
+//     }
+
+//     // summarize results of the game
+//     if (roundsPlayed) {
+//         let summary = `User Wins: ${userWins}\nPC Wins: ${pcWins}\nTies: ${roundsPlayed - userWins - pcWins}`
+//         let status = (userWins === pcWins) ? "It's a tie!" : (userWins > pcWins) ? "You won!" : "You lost!"      
+//         alert(`${status}\n\n${summary}`)        
+//     }
+// }
+
+const btns = document.querySelectorAll('button')
+btns.forEach(btn => btn.addEventListener('click', playRoundWithButtons));
